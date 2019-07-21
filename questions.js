354b10b93943b3ca1321345ffb1e8f501a36fe4e
Charla Bigelow <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-07-20 22:51:09 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+
+const STORE = [{
+    question: 'You value your status as an informed person who always watches the news and stays up to date on local and world politics via the internet and social media. However, you are often afraid and imagine that crime rates are out of control. Your mind is using the:',
+    answers: {
+       a:'Depressive Realism heuristic',
+       b:'Availability heuristic',
+       c:'Representativeness heuristic',
+       d:'Anchoring and Adjustment heuristic'
+    },
+    correctAnswer: 'b'
+    },
+
+    {
+    question: 'Certain words and phrases have powerful emotional affects, when your stomache clenches in dread at the word “cancer” or you feel a sense of unease from the word “death”. What shortcut is your mind using?',
+    answers: {
+       a:'Affect heuristic',
+       b:'Terror Management Theory',
+       c:'Scapegoat Theory',
+       d:'Gut Reaction heuristic'
+    },
+    correctAnswer: 'a'
+    },
+        
+    {
+    question: 'You and your buddy Larry are flipping coins for a probability experiment. Somehow you’ve come up with heads five times in a row! “That’s it,” Larry says. “The next one has got to be tails. It’s statistically impossible!”. Larry is using the:',
+    answers: {
+        a:'Law of the instrument effect',
+        b:'Statistical Bias',
+        c:'Gambler Fallacy',
+        d:'50/50 Theory'
+    },
+    correctAnswer: 'c'
+    },
+
+    {    
+    question: 'The speaker at the cat flatterer convention is asking for audience members to guess the value of an ultra deluxe set of premium cat furniture. “How much do you think it’s worth?” he asks the audience. “At least 3k, right?” the tendency for the audience to then venture guesses closer in range to the speakers represents the:',
+    answers: {   
+        a:'Attentional Bias',
+        b:'Fluency Heuristic',
+        c:'Anchoring Heuristic',
+        d:'Speaker Fallacy'
+    },
+    correctAnswer: 'c'
+    },
+    
+    {
+    question: 'The tendency for people to value that which is communicated more skillfully or elegantly represents the:',
+    answers: {
+        a:'Anchoring Heuristic',
+        b:'Fluency Heuristic',
+        c:'Brain in a Vat',
+        d:'Same Bias'
+    },     
+    correctAnswer: 'b'
+    },
+
+    {
+    question: 'The tendency to ignore (generic, general information) and focus on specific information is the:',
+    answers: {
+        a:'Simulation heuristic.',
+        b:'Gambler’s Fallacy',
+        c:'Impact Bias',
+        d:'Base rate fallacy'
+    },
+    correctAnswer: 'd'
+    },
+    
+    {
+    question: 'The horse you bet on in a race loses by a hair. “It would have been better if it hadn’t been close at all!” you lament, racking your brain over all the small differences that would have let your horse win. This is an example of the:',
+    answers: {
+        a:'Simulation heuristic',
+        b:'Gambler’s Fallacy',
+        c:'Impact Bias',
+        d:'Learned Helplessness'
+    },
+    correctAnswer: 'a'
+    },
+    
+    {
+    question: 'The tendency for a person to recognize and remember details that confirm preconceived notions is an example of:',
+    answers: {
+        a:'Simulation heuristic.',
+        b:'Observer-expectancy Effect',
+        c:'Optimism Bias',
+        d:'Confirmation bias'
+    },
+    correctAnswer: 'd'
+    },
+    
+    {
+    question: 'The tendency to prescribe a person’s apparent behavior to their personality and to ignore circumstance is the:',
+    answers: {
+        a:'Simulation heuristic.',
+        b:'Gambler Fallacy',
+        c:'Fundamental Attribution Error',
+        d:'Learned Helplessness',
+
+    },     
+    correctAnswer: 'c'
+    },
+    
+    {
+    question: 'The tendency for an individual to attribute the overall validity of a decision based on the eventual outcome is known as',
+    answers: {
+        a:'Outcome Bias',
+        b:'Normalcy Bias',
+        c:'Omission Bias',
+        d:'Learned Helplessness'
+    },
+    correctAnswer: 'a'
+    }
+];
